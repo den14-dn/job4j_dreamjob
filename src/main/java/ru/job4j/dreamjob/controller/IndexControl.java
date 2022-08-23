49e36f5f3a5173a39f36e53580592f27fa4b4ca2
den14-dn <den14-dn@yandex.ru>
2022-08-23 08:45:03 +0300

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,12 +1,12 @@
 package ru.job4j.dreamjob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class IndexControl {
     @GetMapping("/index")
     public String index() {
-        return "Greetings from Spring Boot";
+        return "index";
     }
 }

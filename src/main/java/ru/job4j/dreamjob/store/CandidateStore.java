427dcadb2f0e8bef037b6bc52db1498b6829147f
den14-dn <den14-dn@yandex.ru>
2022-08-23 12:05:27 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Developer 1", "I am junior developer"));
+        candidates.put(2, new Candidate(2, "Developer 2", "I am senior developer"));
+        candidates.put(3, new Candidate(3, "Developer 3", "I am junior developer"));
+        candidates.put(4, new Candidate(4, "Developer 4", "I am middle developer"));
+        candidates.put(5, new Candidate(5, "Developer 5", "I am junior developer"));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}

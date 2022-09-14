2bf319fc767cb90aa3117b7c54bcdd194f725cdc
den14-dn <den14-dn@yandex.ru>
2022-09-14 23:16:40 +0300

    2. Связь слоев через Spring DI. [#504856 #343145]

--- src/main/java/ru/job4j/dreamjob/controller/PostControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostControl.java
@@ -6,22 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.job4j.dreamjob.store.PostStore;
+import ru.job4j.dreamjob.service.PostService;
 import ru.job4j.dreamjob.model.Post;
 import net.jcip.annotations.ThreadSafe;
 
 @Controller
 @ThreadSafe
 public class PostControl {
-    private final PostStore store;
-
-    public PostControl(PostStore store) {
-        this.store = store;
-    }
+    private final PostService service = PostService.instOf();
 
     @GetMapping("/posts")
     public String posts(Model model) {
-        model.addAttribute("posts", store.findAll());
+        model.addAttribute("posts", service.findAll());
         return "posts";
     }
 
@@ -32,19 +28,19 @@ public class PostControl {
 
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
-        store.add(post);
+        service.add(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/formUpdatePost/{postId}")
     public String formUpdatePost(Model model, @PathVariable("postId") int id) {
-        model.addAttribute("post", store.findById(id));
+        model.addAttribute("post", service.findById(id));
         return "updatePost";
     }
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
-        store.update(post);
+        service.update(post);
         return "redirect:/posts";
     }
 }
